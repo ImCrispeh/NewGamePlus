d7a715631a40d1818fc6cc980ed6101036d561ad
Christopher Fung <chrisfung2897@hotmail.com>
2019-05-08 23:12:32 +1000

    Initial commit

---
+++ Assets/Scripts/Mob/BaseMob.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BaseMob : NetworkBehaviour {
+
+    public int sightRange;
+    public int attackRange;
+
+    public int baseSpeed;
+    private int speed;
+    private GameObject target;
+
+    private Vector3 newPosition;
+    private Vector3 spawnPosition;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        speed = baseSpeed;
+        spawnPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChooseTarget();
+        Move();
+        Attack();
+	}
+
+    private void ChooseTarget()
+    {
+        float distance = sightRange;
+        target = null;
+        foreach(GameObject player in GameObject.FindGameObjectsWithTag("player"))
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) < distance)
+            {
+                distance = Vector3.Distance(player.transform.position, transform.position);
+                target = player;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (target == null)
+        {
+            newPosition = spawnPosition;
+        }
+        else if(Vector3.Distance(target.transform.position, transform.position) > attackRange)
+        {
+            newPosition = target.transform.position;
+        }
+        else
+        {
+            newPosition = transform.position;
+        }
+        RpcMove(newPosition, speed);
+    }
+
+    [ClientRpc]
+    void RpcMove(Vector3 position,float speed)
+    {
+        if (position == transform.position)
+        {
+            animator.SetFloat("animationstate", 0);
+            return;
+        }
+        animator.SetFloat("animationstate", 1);
+        transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
+        transform.LookAt(position,Vector3.zero);
+    }
+
+    private void Attack()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour {
+
+    public int speed;
+    public NetworkIdentity networkIdentity;
+    public GameObject graphics;
+    public Camera camera;
+
+    public Vector3 newposition;
+    [SyncVar]
+    Vector3 syncpos;
+
+
+    private PlayerEvents playerEvents;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            camera.enabled = false;
+            return;
+        }
+        newposition = transform.position;
+        networkIdentity = GetComponent<NetworkIdentity>();
+        playerEvents = GetComponent<PlayerEvents>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        CheckInput();
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        if (isLocalPlayer)
+        {
+            CmdSetDestination(transform.position);
+        }
+        Move();
+    }
+
+    private void Move()
+    {
+        if (transform.position == newposition)
+        {
+            return;  
+        }
+        if (!isLocalPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, syncpos, speed * Time.deltaTime);
+            return;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, newposition, speed * Time.deltaTime);
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "navmesh")
+            {
+                newposition = hit.point;
+            }
+        }
+    }
+
+
+    [Command]
+    void CmdSetDestination(Vector3 pos)
+    {
+        syncpos = pos;
+    }
+}
